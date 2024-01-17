3e279f207b4cb2b95e7c3826d23b22759a1acc82
Kyle Michaels <kyle.michaels@gmail.com>
2024-01-16 20:06:57 -0800

    cleared old code from signup

--- src/components/signup-view/signup-view.jsx
+++ src/components/signup-view/signup-view.jsx
@@ -36,46 +36,6 @@ export const SignupView = () => {
   };
 
   return (
-    // <form onSubmit={handleSubmit}>
-    //   <label>
-    //     Username:
-    //     <input
-    //       type="text"
-    //       value={username}
-    //       onChange={(e) => setUsername(e.target.value)}
-    //       required
-    //       minLength="3"
-    //     />
-    //   </label>
-    //   <label>
-    //     Password:
-    //     <input
-    //       type="password"
-    //       value={password}
-    //       onChange={(e) => setPassword(e.target.value)}
-    //       required
-    //     />
-    //   </label>
-    //   <label>
-    //     Email:
-    //     <input
-    //       type="email"
-    //       value={email}
-    //       onChange={(e) => setEmail(e.target.value)}
-    //       required
-    //     />
-    //   </label>
-    //   <label>
-    //     Birthday:
-    //     <input
-    //       type="date"
-    //       value={birthday}
-    //       onChange={(e) => setBirthday(e.target.value)}
-    //       required
-    //     />
-    //   </label>
-    //   <button type="submit">Submit</button>
-    // </form>
     <Form onSubmit={handleSubmit}>
       {/* Username */}
       <Form.Group controlId="formUsername">
