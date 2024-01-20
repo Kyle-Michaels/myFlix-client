2bc4503ec1c46cffcb7aa93d11b1565ded664fc8
Kyle Michaels <kyle.michaels@gmail.com>
2024-01-19 19:52:25 -0800

    Made redux store available to react app

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import { MainView } from './components/main-view/main-view';
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
 import Container from 'react-bootstrap/Container';
 
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -8,9 +10,11 @@ import "./index.scss";
 // Main component
 const MyFlixApplication = () => {
   return (
-    <Container>
-      <MainView />
-    </Container>
+    <Provider store={store}>
+      <Container>
+        <MainView />
+      </Container>
+    </Provider>
   );
 };
 
